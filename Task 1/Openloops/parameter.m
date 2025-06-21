c2828e2fba52d8f01092819e2f2f06272765f0b3
Henry01batubara <henrygamalielbatubara@gmail.com>
2025-06-21 23:42:03 +0700

    openloops

--- Task 1/Openloops/parameter.m
+++ Task 1/Openloops/parameter.m
@@ -9,4 +9,4 @@ Iout_ripple = 0.2 * IL;
 Vout_ripple = 0.02 * Vo;
 
 L = (Vo * (1 - D)) / (Iout_ripple * fsw);    % 1.35e-4
-C = (Vo * (1 - D)) / (8 * L * fsw^2 * Vout_ripple):    % 2.31e-6
+C = (Vo * (1 - D)) / (8 * L * fsw^2 * Vout_ripple);    % 2.31e-6
